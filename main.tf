c27db90ad745e7c10f981f069f1c3ac7d153d6a9
Pericles S. da C. Jr <pericles.scj@gmail.com>
2020-04-21 19:11:51 -0300

    Remove unused code

--- main.tf
+++ main.tf
@@ -63,11 +63,3 @@ resource "libvirt_domain" "test" {
     autoport = true
   }
 }
-
-
-# Print the IP
-# Can use `virsh domifaddr <vm_name> <interface>` to get the ip later
-#output "ips" {
-#  # show IP, run 'terraform refresh' if not populated
-#  value = libvirt_domain.test.*.network_interface.0.addresses
-#}
