105265e4714e358b2b82699c9db36ae65d88b6ed
Pericles S. da C. Jr <pericles.scj@gmail.com>
2020-04-21 18:52:01 -0300

    Add output.tf

---
+++ output.tf
@@ -0,0 +1,6 @@
+# Print the IP
+# Can use `virsh domifaddr <vm_name> <interface>` to get the ip later
+output "ips" {
+  # show IP, run 'terraform refresh' if not populated
+  value = libvirt_domain.test.*.network_interface.0.addresses
+}
