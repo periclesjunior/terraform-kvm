0f2581bc02930f266623f20ff2d9ff8dc64cead2
Pericles S. da C. Jr <pericles.scj@gmail.com>
2020-05-10 18:07:16 -0300

    Remove old comment

--- output.tf
+++ output.tf
@@ -2,6 +2,5 @@
 # Can use `virsh domifaddr <vm_name> <interface>` to get the ip later
 output "ips" {
   # show IP, run 'terraform refresh' if not populated
-  #value = libvirt_domain.test.*.network_interface.0.addresses
   value = libvirt_domain.ubuntu.*.network_interface.0.addresses
 }
